be50086ac21904ec00447b03f29491050a5f2c35
Vitalforce88 <brian1694@gmail.com>
2021-02-18 22:29:30 +0300

    Исправил домашку к уроку 6, так и не осилил 3е задание...

--- Lesson_6/main.cpp
+++ Lesson_6/main.cpp
@@ -1,64 +1,83 @@
+/* Егоров А.В. Работа с файлами и памятью
+1.Написать программу, которая создаст два текстовых файла,
+  примерно по 50-100 символов в каждом (особого значения не имеет);
+2.Написать функцию, «склеивающую» эти файлы, предварительно буферизуя
+  их содержимое в динамически выделенный сегмент памяти нужного размера.
+3.* Написать программу, которая проверяет присутствует ли указанное пользователем
+    при запуске программы слово в указанном пользователем файле (для простоты работаем только с латиницей).
+*/
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
+void file2fileText(const string& from, const string& to){
+    ofstream ofs(to,_S_app);
+    ifstream ifs(from);
+
+    char c;
+    while(ifs.get(c) && !ifs.eof()){
+        ofs << c;
+    }
+}
+void fileprint(const string& file){
+    fstream fs{file};
+    char c;
+    while(fs.get(c) && !fs.eof()){
+        cout << c;
+    }
+}
+
+//bool find(const string& file, const string word)
 
 int main()
 {
-    ofstream fos1("file_1.txt");
-    ofstream fos2("file_2.txt");
+    fstream potok1("file_1.txt");
+    fstream potok2("file_2.txt");
 
-    fos1 << "C++ (/ˌsiːˌplʌsˈplʌs/) is a general-purpose programming language "
-            "created by Bjarne Stroustrup as an extension of the C programming language,"
-            "or << C with Classes >>. The language has expanded significantly over time,"
-            "and modern C++ now has object-oriented, generic, and functional features"
-            "in addition to facilities for low-level memory manipulation."
-            "It is almost always implemented as a compiled language, and many vendors provide C++ compilers,"
-            "including the Free Software Foundation, LLVM, Microsoft, Intel, Oracle, and IBM,"
-            "so it is available on many platforms";
+    potok1 << "C++ (/ˌsiːˌplʌsˈplʌs/) is a general-purpose programming language\n"
+            "created by Bjarne Stroustrup as an extension of the C programming language,\n"
+            "or << C with Classes >>. The language has expanded significantly over time,\n"
+            "and modern C++ now has object-oriented, generic, and functional features\n"
+            "in addition to facilities for low-level memory manipulation.\n"
+            "It is almost always implemented as a compiled language, and many vendors provide C++ compilers,\n"
+            "including the Free Software Foundation, LLVM, Microsoft, Intel, Oracle, and IBM,\n"
+            "so it is available on many platforms\n";
 
-    fos2 << "C++ was designed with an orientation toward system programming and embedded,"
-            "resource-constrained software and large systems, with performance, efficiency,"
-            "and flexibility of use as its design highlights.[10] C++ has also been found useful "
-            "in many other contexts, with key strengths being software infrastructure and resource-constrained "
-            "applications,[10] including desktop applications, video games, servers (e.g. e-commerce, web search, "
-            "or SQL servers), and performance-critical applications (e.g. telephone switches or space probes).";
-    fos1.close();
-    fos2.close();
+    potok2 << "C++ was designed with an orientation toward system programming and embedded,\n"
+            "resource-constrained software and large systems, with performance, efficiency,\n"
+            "and flexibility of use as its design highlights.[10] C++ has also been found useful\n"
+            "in many other contexts, with key strengths being software infrastructure and resource-constrained\n"
+            "applications,[10] including desktop applications, video games, servers (e.g. e-commerce, web search,\n"
+            "or SQL servers), and performance-critical applications (e.g. telephone switches or space probes).\n";
 
-    ifstream fis1("file_1.txt");
-
-    ifstream fis2("file_2.txt");
-
-    string s1;
-    string s2;
-
-    getline(fis1,s1);
-    getline(fis2,s2);
-    string s3 = s1 + s2;
-    cout << s3;
-    fis1.close();
-    fis2.close();
+    //potok1.close();
+    potok2.close();
 
 
-/*    char *ptr;
-    ptr = new char[500];
-    for(int i = 0; i < 500 ; i++){
-        fis1 >> ptr[i];
-    }
-    for (int i = 0; i < 500; i++){
-        cout << ptr[i];
-    };
+vector <char> v;
+for (char s; potok1 >> s;){
+v.push_back(s);
+}
+//string f = "Bjarne";
+// auto result = find(v.begin(),v.end(),f);
+//if (result != v.end()){
+//cout << "find!";
+//}
+//else cout << "not found";
+//for(char c : v) cout << c;
+    file2fileText("file_2.txt","file_1.txt");
+    fileprint("file_1.txt");
 
-    char *ptr2;
 
-    ptr2 = new char[500];
-    for(int i = 0; i < 500 ; i++){
-        fis2 >> ptr[i];
-    }
+//ifstream isf("file_1.txt");
+//string strtext;
+//getline(isf,strtext);
+//cout << strtext;
+//string f = "C++";
 
-    delete [] ptr;
-    delete [] ptr2;
-*/
+//find(strtext.begin(),strtext.end(),f);
+
     return 0;
 }
